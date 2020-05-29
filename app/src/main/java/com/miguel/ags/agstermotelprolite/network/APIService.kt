1c087ddd4f8d4783c8a0fc7383644f472e6f0cd0
mangelm <miguelmanas.ags@gmail.com>
2020-05-29 14:01:04 +0200

    Preparación de carga en el spinner de las camaras

--- app/src/main/java/com/miguel/ags/agstermotelprolite/network/APIService.kt
+++ app/src/main/java/com/miguel/ags/agstermotelprolite/network/APIService.kt
@@ -1,6 +1,5 @@
 package com.miguel.ags.agstermotelprolite.network
 
-import com.miguel.ags.agstermotelprolite.data.model.Camaras
 import com.miguel.ags.agstermotelprolite.data.model.Usuarios
 import retrofit2.Call
 import retrofit2.http.*
@@ -28,8 +27,7 @@ interface APIService {
     ): retrofit2.Call<Usuarios>
 
     @GET("usuarios")
-    open fun getCamaras(): Call<List<Camaras>>
-
+    open fun getCamarasById(@Query("id") id: Int): Call<Usuarios>
 
 }
 
--- app/src/main/java/com/miguel/ags/agstermotelprolite/ui/home/SpinnerViewModel.kt
+++ app/src/main/java/com/miguel/ags/agstermotelprolite/ui/home/SpinnerViewModel.kt
@@ -16,41 +16,11 @@ class SpinnerViewModel : ViewModel() {
 
     lateinit var camaras: List<Camaras>
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState);
-//;
-//        val languages = resources.getStringArray(R.array.sp)
-//
-//        // access the spinner
-//        val spinner = findViewById<Spinner>(R.id.)
-//        if (spinner != null) {
-//            val adapter = ArrayAdapter(this,
-//                R.layout.simple_spinner_item, languages)
-//            spinner.adapter = adapter
-//
-//            spinner.onItemSelectedListener = object :
-//                AdapterView.OnItemSelectedListener {
-//                override fun onItemSelected(parent: AdapterView<*>,
-//                                            view: View, position: Int, id: Long) {
-//                    Toast.makeText(this@SpinnerViewModel,
-//                        getString(R.string.selected_item) + " " +
-//                                "" + languages[position], Toast.LENGTH_SHORT).show()
-//                }
-//
-//                override fun onNothingSelected(parent: AdapterView<*>) {
-//                    // write code to perform some action
-//                }
-//            }
-//        }
-//    }
-
     fun loadSpinnerData() {
         val purApp = RetrofitClient.getRetrofitInstance().create(APIService::class.java)
 
-        val releaseResponse = purApp.getCamaras().execute()
-        val releases = releaseResponse.body()
+        val releaseResponse = purApp.getCamarasById(0).execute()
 
-        val result = ArrayList<Camaras>()
 
     }
 
